dcc73c1c43e69c87b54dc782f922eff0054492f1
dutexion <dutexion@gmail.com>
2024-01-17 03:30:06 +0900

    모노레포 구축

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
