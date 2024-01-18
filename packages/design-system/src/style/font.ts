f6fdd443ea3602897ca1e9bdd6d840d6ae27d1a5
dutexion <dutexion@gmail.com>
2024-01-19 00:34:01 +0900

    chore :: 폰트 추가

--- packages/design-system/src/style/font.ts
+++ packages/design-system/src/style/font.ts
@@ -20,6 +20,10 @@ export const font = {
   body1: fontGenerator(18, 400, 0.15),
   body2: fontGenerator(16, 400, 0.15),
 
+  buttonLarge: fontGenerator(20, 600, 0.15),
+  buttonMedium: fontGenerator(18, 600, 0.15),
+  buttonSmall: fontGenerator(16, 600, 0.15),
+
   caption: fontGenerator(16, 400, 0.15),
   label: fontGenerator(16, 500, 0.15),
 } as const;
