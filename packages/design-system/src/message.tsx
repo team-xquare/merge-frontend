eb837af2e246adefafc5ea2ffcf3f06638175f54
dutexion <dutexion@gmail.com>
2024-01-18 11:12:54 +0900

    chore :: project setting

--- packages/design-system/src/message.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from 'react';
-
-type Props = {
-  message: string;
-  children?: never;
-};
-
-export const Message: FC<Props> = ({ message }) => <a>{message}</a>;
--- packages/design-system/src/test.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC, ReactNode } from 'react';
-
-type Props = {
-  children: ReactNode;
-};
-
-export const Test: FC<Props> = ({ children }) => <p>{children}</p>;
