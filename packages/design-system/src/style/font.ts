82cd8708ed19c28ceab65a4e7b74cbca4a170452
dutexion <dutexion@gmail.com>
2024-01-18 12:29:18 +0900

    design :: 폰트 틀 잡음

---
+++ packages/design-system/src/style/font.ts
@@ -0,0 +1,29 @@
+const fontGenerator = (size: number, weight: number, letterSpace: number) =>
+  `
+  font-size: ${size}px;
+  font-weight: ${weight};
+  letter-spacing: ${letterSpace}px;
+`;
+
+// bold = 700 , semi-bold = 600 , medium = 500 , reqular = 400
+
+export const font = {
+  heading1: fontGenerator(64,700,-0.96),
+  heading2: fontGenerator(,,),
+  heading3: fontGenerator(,,),
+  heading4: fontGenerator(,,),
+  heading5: fontGenerator(,,),
+  heading6: fontGenerator(,,),
+  
+  subTitle1: fontGenerator(,,),
+  subTitle2: fontGenerator(,,),
+  subTitle3: fontGenerator(,,),
+
+  body1: fontGenerator(,,);
+  body2: fontGenerator(,,);
+
+  caption: fontGenerator(,,);
+  label: fontGenerator(,,);
+} as const;
+
+export type fontKeyOfType = keyof typeof font;
