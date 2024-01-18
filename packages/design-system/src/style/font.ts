1a68694ceeb171abefb56e9c79aa5ef9142a0f30
dutexion <dutexion@gmail.com>
2024-01-19 00:32:16 +0900

    style :: 주석 제거와 코드 스타일링

--- packages/design-system/src/style/font.ts
+++ packages/design-system/src/style/font.ts
@@ -2,28 +2,26 @@ const fontGenerator = (size: number, weight: number, letterSpace: number) =>
   `
   font-size: ${size}px;
   font-weight: ${weight};
-  letter-spacing: ${(size/100)*letterSpace}px;
+  letter-spacing: ${(size / 100) * letterSpace}px;
 `;
 
-// bold = 700 , semi-bold = 600 , medium = 500 , reqular = 400
-
 export const font = {
-  heading1: fontGenerator(64,700,-1.5),
-  heading2: fontGenerator(56,600,-0.5),
-  heading3: fontGenerator(48,600,0),
-  heading4: fontGenerator(32,600,0.25),
-  heading5: fontGenerator(32,600,0),
-  heading6: fontGenerator(20,600,0.15),
-  
-  subTitle1: fontGenerator(20,600,0.15),
-  subTitle2: fontGenerator(18,600,0.15),
-  subTitle3: fontGenerator(14,600,0.1),
+  heading1: fontGenerator(64, 600, -1.5),
+  heading2: fontGenerator(56, 600, -0.5),
+  heading3: fontGenerator(48, 600, 0),
+  heading4: fontGenerator(32, 600, 0.25),
+  heading5: fontGenerator(32, 600, 0),
+  heading6: fontGenerator(20, 600, 0.15),
 
-  body1: fontGenerator(18,400,0.15),
-  body2: fontGenerator(16,400,0.15),
+  subTitle1: fontGenerator(20, 600, 0.15),
+  subTitle2: fontGenerator(18, 600, 0.15),
+  subTitle3: fontGenerator(14, 600, 0.1),
 
-  caption: fontGenerator(16,400,0.15),
-  label: fontGenerator(16,500,0.15)
+  body1: fontGenerator(18, 400, 0.15),
+  body2: fontGenerator(16, 400, 0.15),
+
+  caption: fontGenerator(16, 400, 0.15),
+  label: fontGenerator(16, 500, 0.15),
 } as const;
 
 export type fontKeyOfType = keyof typeof font;
