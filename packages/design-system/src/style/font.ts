586907b58cb8ba230a8a5b31027120b5549c4453
heedda <heedda.kollim@gmail.com>
2024-01-19 00:16:50 +0900

    design :: font 정리

--- packages/design-system/src/style/font.ts
+++ packages/design-system/src/style/font.ts
@@ -9,21 +9,21 @@ const fontGenerator = (size: number, weight: number, letterSpace: number) =>
 
 export const font = {
   heading1: fontGenerator(64,700,-1.5),
-  heading2: fontGenerator(,,),
-  heading3: fontGenerator(,,),
-  heading4: fontGenerator(,,),
-  heading5: fontGenerator(,,),
-  heading6: fontGenerator(,,),
+  heading2: fontGenerator(56,600,-0.5),
+  heading3: fontGenerator(48,600,0),
+  heading4: fontGenerator(32,600,0.25),
+  heading5: fontGenerator(32,600,0),
+  heading6: fontGenerator(20,600,0.15),
   
-  subTitle1: fontGenerator(,,),
-  subTitle2: fontGenerator(,,),
-  subTitle3: fontGenerator(,,),
+  subTitle1: fontGenerator(20,600,0.15),
+  subTitle2: fontGenerator(18,600,0.15),
+  subTitle3: fontGenerator(14,600,0.1),
 
-  body1: fontGenerator(,,);
-  body2: fontGenerator(,,);
+  body1: fontGenerator(18,400,0.15),
+  body2: fontGenerator(16,400,0.15),
 
-  caption: fontGenerator(,,);
-  label: fontGenerator(,,);
+  caption: fontGenerator(16,400,0.15),
+  label: fontGenerator(16,500,0.15)
 } as const;
 
 export type fontKeyOfType = keyof typeof font;
