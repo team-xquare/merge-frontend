40a92112024bbb3b9acae2db5a2d9985f43683cd
dutexion <dutexion@gmail.com>
2024-01-19 13:51:30 +0900

    chore :: Button export

--- packages/design-system/src/index.tsx
+++ packages/design-system/src/index.tsx
@@ -1,1 +1,2 @@
 export { StyledProvider } from './style/StyledProvider';
+export { Button } from './components/Button';
--- packages/user/src/App.tsx
+++ packages/user/src/App.tsx
@@ -1,5 +1,18 @@
-import { StyledProvider } from '@merge/design-system';
+import { StyledProvider, Button } from '@merge/design-system';
 
 export default function App() {
-  return <StyledProvider>asd</StyledProvider>;
+  return (
+    <StyledProvider>
+      <Button
+        buttonStyle="solid"
+        onClick={() => {
+          console.log(13);
+        }}
+        size="large"
+        isDisable={false}
+      >
+        버튼
+      </Button>
+    </StyledProvider>
+  );
 }
