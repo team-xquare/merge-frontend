4e83bc14bde1c30ac7cc1e5d85cbe7e01a3fe82e
dutexion <dutexion@gmail.com>
2024-01-30 21:38:41 +0900

    fix :: header outlet

--- packages/user/src/components/Header/index.tsx
+++ packages/user/src/components/Header/index.tsx
@@ -2,13 +2,17 @@ import styled from '@emotion/styled';
 import { theme } from '@merge/design-system';
 import { Logo } from './Logo';
 import { Menu } from './Menu';
+import { Outlet } from 'react-router-dom';
 
 export const Header = () => {
   return (
-    <Wrapper>
-      <Logo />
-      <Menu isLogin={true} />
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Logo />
+        <Menu isLogin={true} />
+      </Wrapper>
+      <Outlet />
+    </>
   );
 };
 
