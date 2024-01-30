4d7cc1d58399486cb60522375e0fcdf0cdca4a6c
dutexion <dutexion@gmail.com>
2024-01-30 21:41:56 +0900

    feat :: sign page publish

--- packages/user/src/pages/Sign.tsx
+++ packages/user/src/pages/Sign.tsx
@@ -1,3 +1,61 @@
+import styled from '@emotion/styled';
+import sign from '../assets/sign.svg';
+import { theme, Input, Button } from '@merge/design-system';
+
 export const Sign = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Container1>
+        <img src={sign} />
+      </Container1>
+      <Container2>
+        <Title>
+          <strong>Log In</strong>
+          <div>스퀘어 계정으로 로그인 해주세요.</div>
+        </Title>
+        <Input width={400} placeholder="아이디" margin={['top', 44]} />
+        <Input width={400} placeholder="아이디" margin={['top', 48]} />
+        <BtnContainer>
+          <Button buttonStyle="solid" margin={['top', 52]} size="medium">
+            로그인
+          </Button>
+        </BtnContainer>
+      </Container2>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 100vw;
+  display: flex;
+`;
+
+const Container1 = styled.div`
+  width: 945px;
+  height: calc(100vh - 52px);
+  overflow: hidden;
+`;
+
+const Container2 = styled.div`
+  width: calc(1920px - 945px);
+  padding-top: 264px;
+  padding-left: 258px;
+`;
+
+const Title = styled.div`
+  strong {
+    color: ${theme.color.primary500};
+    ${theme.font.heading2}
+  }
+  color: ${theme.color.primary600};
+  ${theme.font.subTitle3}
+  div {
+    margin-top: 6px;
+  }
+`;
+
+const BtnContainer = styled.div`
+  width: 400px;
+  display: flex;
+  justify-content: end;
+`;
