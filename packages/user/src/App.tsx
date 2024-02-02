416e307873ae8ae3f9273d2e7999854576dd0def
dutexion <dutexion@gmail.com>
2024-02-02 20:08:57 +0900

    fix :: 시도

--- packages/user/src/App.tsx
+++ packages/user/src/App.tsx
@@ -1,11 +1,7 @@
 import { StyledProvider } from '@merge/design-system';
-import { RouterProvider } from 'react-router-dom';
-import { Router } from './router';
+// import { RouterProvider } from 'react-router-dom';
+// import { Router } from './router';
 
 export default function App() {
-  return (
-    <StyledProvider>
-      <RouterProvider router={Router} />
-    </StyledProvider>
-  );
+  return <StyledProvider>이렇게라도 해봐야지{/* <RouterProvider router={Router} /> */}</StyledProvider>;
 }
