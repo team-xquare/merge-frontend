5042295c2f2d218abd9686a7c2623123032d8cb2
dutexion <dutexion@gmail.com>
2024-02-02 20:13:04 +0900

    rollback

--- packages/user/src/App.tsx
+++ packages/user/src/App.tsx
@@ -1,7 +1,11 @@
 import { StyledProvider } from '@merge/design-system';
-// import { RouterProvider } from 'react-router-dom';
-// import { Router } from './router';
+import { RouterProvider } from 'react-router-dom';
+import { Router } from './router';
 
 export default function App() {
-  return <StyledProvider>이렇게라도 해봐야지{/* <RouterProvider router={Router} /> */}</StyledProvider>;
+  return (
+    <StyledProvider>
+      <RouterProvider router={Router} />
+    </StyledProvider>
+  );
 }
