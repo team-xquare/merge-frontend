67b1bd23fb2395b495b9b095dd96662fd5919bbc
dutexion <dutexion@gmail.com>
2024-02-02 17:06:03 +0900

    fix :: 라우터 없으면 될까

--- packages/user/src/App.tsx
+++ packages/user/src/App.tsx
@@ -1,11 +1,7 @@
 import { StyledProvider } from '@merge/design-system';
 import { RouterProvider } from 'react-router-dom';
-import { Router } from './router';
+// import { Router } from './router';
 
 export default function App() {
-  return (
-    <StyledProvider>
-      <RouterProvider router={Router} />
-    </StyledProvider>
-  );
+  return <StyledProvider>{/* <RouterProvider router={Router} /> */}f되나?</StyledProvider>;
 }
