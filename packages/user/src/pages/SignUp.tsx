0d1e27927cdc1d8ebb3b8729bdb90404989532df
dutexion <dutexion@gmail.com>
2024-02-05 23:21:09 +0900

    feat :: signup page 임시 완성

--- packages/user/src/pages/SignUp.tsx
+++ packages/user/src/pages/SignUp.tsx
@@ -3,11 +3,26 @@ import SignImg from '../assets/sign.svg';
 import { theme, Input, Button } from '@merge/design-system';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { signupType } from '../types/signType';
+import { dataWhiteSpace } from '../func/dataWhiteSpace';
+import { signUp } from 'src/apis/sign';
+
+interface signupInputType extends signupType {
+  okPassword: string;
+}
 
 export const SignUp = () => {
-  const [data, setData] = useState({ id: '', password: '' });
+  const [data, setData] = useState<signupInputType>({
+    student_name: '',
+    github: '',
+    password: '',
+    school_gcn: '',
+    email: '',
+    account_id: '',
+    okPassword: '',
+  });
 
-  const { id, password } = data;
+  const { student_name, github, password, school_gcn, email, account_id, okPassword } = data;
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -18,11 +33,19 @@ export const SignUp = () => {
   };
 
   const onClick = () => {
-    console.log(data);
+    signUp(data)
+      .then((res) => console.log(res))
+      .catch((err) => console.error(err));
+  };
+
+  const samePassword = () => {
+    if (password === okPassword) return true;
   };
 
   const canSubmit = () => {
-    if (id === '' || password === '') return true;
+    if (dataWhiteSpace(data)) {
+      return samePassword();
+    }
   };
 
   return (
@@ -39,21 +62,73 @@ export const SignUp = () => {
         </Title>
         <Input
           width={400}
-          placeholder="아이디"
+          label="이름"
+          placeholder="이름"
           margin={['top', 44]}
           type="text"
-          name="id"
+          name="student_name"
           onChange={onChange}
-          value={id}
+          value={student_name}
         />
         <Input
           width={400}
-          placeholder="비밀번호"
+          label="학번"
+          placeholder="학번"
+          margin={['top', 44]}
+          type="text"
+          name="school_gcn"
+          onChange={onChange}
+          value={school_gcn}
+        />
+        <Input
+          width={400}
+          label="이메일"
+          placeholder="dsm 계정의 이메일"
+          margin={['top', 44]}
+          type="text"
+          name="email"
+          onChange={onChange}
+          value={email}
+        />
+        <Input
+          width={400}
+          label="아이디"
+          placeholder="아이디"
+          margin={['top', 44]}
+          type="text"
+          name="account_id"
+          onChange={onChange}
+          value={account_id}
+        />
+        <Input
+          width={400}
+          label="깃허브 주소"
+          placeholder="링크"
+          margin={['top', 44]}
+          type="text"
+          name="github"
+          onChange={onChange}
+          value={github}
+        />
+        <Input
+          width={400}
+          label="비밀번호"
+          placeholder="숫자, 특수 문자 1자 이상 영어 대소문자 포함 8~15자"
           margin={['top', 48]}
           type="password"
           name="password"
           onChange={onChange}
           value={password}
+        />
+        <Input
+          width={400}
+          label="비밀번호 확인"
+          placeholder="한 번 더 입력해주세요"
+          margin={['top', 48]}
+          type="password"
+          name="okPassword"
+          onChange={onChange}
+          value={okPassword}
         />
         <BtnContainer>
           <Button buttonStyle="solid" margin={['top', 52]} size="medium" onClick={onClick} isDisable={canSubmit()}>
