00aa53effb4ff41c472ebd9f0cf51e817e597280
dutexion <dutexion@gmail.com>
2024-02-12 13:24:37 +0900

    design :: registration publish

--- packages/user/src/pages/Registration.tsx
+++ packages/user/src/pages/Registration.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState, ReactElement } from 'react';
 import styled from '@emotion/styled';
 import { SubHeader } from '../components/Registration/SubHeader';
 import { Progress } from '../components/Registration/Progress';
+import {
+  RegisterFormFirst,
+  RegisterFormSecond,
+  RegisterFormThird,
+  RegisterFormForth,
+} from '../components/Registration/Form';
+
+const registerFormArray: ReactElement[] = [
+  <RegisterFormFirst />,
+  <RegisterFormSecond />,
+  <RegisterFormThird />,
+  <RegisterFormForth />,
+];
+
+type pageKindType = 'register' | 'deploy';
 
 export const Registration = () => {
+  const [progress, setProgress] = useState<number>(0);
+
   return (
     <Wrapper>
       <SubHeader />
       <Container>
-        <Progress />
+        <Progress progress={progress} kind={'register'} />
+        {registerFormArray[progress]}
       </Container>
     </Wrapper>
   );
@@ -20,6 +39,8 @@ const Wrapper = styled.div`
 
 const Container = styled.div`
   height: calc(100% - 80px);
-  padding: 40px 371px 0 280px;
+  padding: 40px 394px;
   overflow: auto;
+  display: flex;
+  justify-content: space-between;
 `;
