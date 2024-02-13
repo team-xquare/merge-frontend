96585d516d238406434fd6d03396919c084adae5
dutexion <dutexion@gmail.com>
2024-02-13 19:08:45 +0900

    create :: project 등록 type

---
+++ packages/user/src/types/projectType.ts
@@ -0,0 +1,10 @@
+export type projectType = {
+  project_name_ko: string;
+  project_name_en: string;
+  team_name_en: string;
+  description: string;
+  github_url: string;
+  web_url: string;
+  play_store_url: string;
+  app_store_url: string;
+};
