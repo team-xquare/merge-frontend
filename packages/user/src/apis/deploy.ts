fa18ea9bccb80f252796646de536652b4aa3a996
dutexion <dutexion@gmail.com>
2024-02-14 20:40:49 +0900

    feat :: 배포와 중복확인 연동

---
+++ packages/user/src/apis/deploy.ts
@@ -0,0 +1,32 @@
+import { instance } from './axios';
+import { deployType } from '../types/projectType';
+import { toast } from 'react-toastify';
+
+export const deploy = async (data: deployType) => {
+  const { container_name, github_url, service_type, redis, mysql } = data;
+
+  const postData = {
+    container_name,
+    github_url,
+    service_type,
+    project_id: 'c842b3a0-59a0-4c74-9674-695e1701a19c',
+    use_database: {
+      redis,
+      mysql,
+    },
+  };
+
+  console.log(postData);
+
+  return await instance.post('/deploy', postData);
+};
+
+export const duplication = async (name: string) => {
+  const result = await instance.get(`/deploy/duplicate?container-name=${name}`);
+
+  if (!result.data) {
+    toast.success('사용되지 않은 컨테이너 명입니다.');
+  } else {
+    toast.error('이미 사용된 컨테이너 명입니다.');
+  }
+};
