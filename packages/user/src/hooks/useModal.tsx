b517eaab4cdee9d4e88080a86bbc1e9d9a8ec171
dutexion <dutexion@gmail.com>
2024-02-16 01:06:40 +0900

    feat :: useModal

---
+++ packages/user/src/hooks/useModal.tsx
@@ -0,0 +1,24 @@
+import { useCallback, useState } from 'react';
+
+export type useModalReturnType = {
+  visible: boolean;
+  show: () => void;
+  close: () => void;
+};
+
+export type useModalProps = {
+  defaultVisible?: boolean;
+};
+
+export const useModal = ({ defaultVisible = false }: useModalProps = {}): useModalReturnType => {
+  const [visible, setVisible] = useState(defaultVisible);
+
+  const show = useCallback(() => setVisible(true), [visible]);
+  const close = useCallback(() => setVisible(false), [visible]);
+
+  return {
+    visible,
+    show,
+    close,
+  };
+};
