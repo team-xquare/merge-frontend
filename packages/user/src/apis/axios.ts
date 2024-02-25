34b4c2288cb8adcccf5a232a845e4cbc1807286d
dutexion <dutexion@gmail.com>
2024-02-25 16:39:36 +0900

    feat :: reissue

--- packages/user/src/apis/axios.ts
+++ packages/user/src/apis/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { Cookie } from '../utils/cookie';
 import { toast } from 'react-toastify';
+import { reissue } from './sign';
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_BASE_URL,
@@ -25,14 +26,21 @@ instance.interceptors.response.use(
   },
   (err) => {
     const {
-      //config,
       response: { status },
     } = err;
     if (status === 403 || status === 401) {
-      //const token = Cookie.get('refreshToken');
-      Cookie.remove('accessToken');
-      Cookie.remove('refreshToken');
-      window.location.href = '/signin';
+      const token = Cookie.get('refreshToken');
+      reissue(token)
+        .then((res) => {
+          Cookie.set('accessToken', res.data.access_token);
+          Cookie.set('refreshToken', res.data.refresh_token);
+          window.location.reload();
+        })
+        .catch(() => {
+          Cookie.remove('accessToken');
+          Cookie.remove('refreshToken');
+          window.location.href = '/signin';
+        });
     } else {
       toast.error('오류가 발생헀습니다');
       return Promise.reject(err);
